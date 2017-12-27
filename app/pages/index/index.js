9848e9fc5d9cbc639602e3ca18811d66845bc054
Menghua Zhai <ted@cs.uky.edu>
2017-12-26 19:11:35 -0500

    set data with recursive structure

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -10,41 +10,41 @@ Page({
   data: {
     userInfo: {},
     socket: null,
-    is_reserved: false,
-    last_reserv_time: null,
+
+    // system status
+    status: {
+      last_reserve_time: null, // null indicates the bathroom is available
+      otherUsers: []
+    },
+
     debug_str: ""
   },
 
   //事件处理函数
   reserveTap: function () {
-    this.setData({ last_reserve_time: new Date() })
+    this.setData({ 'status.last_reserve_time': new Date() })
   },
 
   onLoad: function () {
     // initialize data
     this.setData({
-      socket: new WxSocket(app.globalData.serverUrl),
-      userInfo: app.globalData.userInfo,
-      last_reserve_time: new Date()})
+      'socket': new WxSocket(app.globalData.serverUrl),
+      'userInfo': app.globalData.userInfo,
+      'status.last_reserve_time': new Date()
+    })
 
     // setup a websocket connection
-    try {
-      this.data.socket.connect()
-      this.setData({debug_str: "connection success" })
-    } catch(connectError) {
-      console.log(connectError)
-      this.setData({debug_str: "connection failed" })
-    }
+    //   this.data.socket.connect()
   },
 
-  // onReady: function() {
+  // onReady: function () {
   //   var that = this
-  //   this.interval = setInterval(function(){
+  //   this.interval = setInterval(function () {
   //     const currTime = new Date()
-  //     that.setData({ debug_str: utils.formatTimeDiff(currTime, that.data.last_reserve_time) })
+  //     that.setData({ 'debug_str': utils.formatTimeDiff(currTime, that.data.status.last_reserve_time) })
   //   }, 1000)
   // },
-  // onUnload: function() {
+  // onUnload: function () {
   //   clearInterval(this.interval)
   // }
 })
